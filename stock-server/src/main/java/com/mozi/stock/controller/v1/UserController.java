2402e0997891321a9f838925a71ea6070f5de3ac
mozi <2138844072@qq.com>
2025-06-10 20:53:39 +0800

    feat(stock): 添加异常处理和日志记录功能
    - 新增自定义异常类 StockException 和异常抛出工具类 Exceptions
    - 实现全局异常处理器 GlobalExceptionHandler
    - 添加 logback 配置文件，设置日志输出格式和存储策略- 在 UserController 中演示自定义异常的使用

--- stock-server/src/main/java/com/mozi/stock/controller/v1/UserController.java
+++ stock-server/src/main/java/com/mozi/stock/controller/v1/UserController.java
@@ -1,6 +1,8 @@
 package com.mozi.stock.controller.v1;
 
 import com.mozi.stock.api.UserControllerAPI;
+import com.mozi.stock.enums.CodeEnum;
+import com.mozi.stock.exception.Exceptions;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,7 +15,7 @@ public class UserController implements UserControllerAPI {
 
   @Override
   public String getName() {
-
+    Exceptions.cast(CodeEnum.TOKEN_PAST_DUE );
     return "itheima";
   }
 }
